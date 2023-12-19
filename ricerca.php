071d7d09fe0682d38c1a2638a3fa0b9547a7cc99
ReDMallard <marco@titori.it>
2023-12-19 01:30:44 +0100

    aggiunge ricerca php

---
+++ ricerca.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search MP</title>
+</head>
+<body>
+    <form action="alberghi.php">
+        <label for="vote">vote:</label>
+        <input type="number" id="vote" name="vote"  />
+        <button type="submit">search</button>
+    </form>
+</body>
+</html>
